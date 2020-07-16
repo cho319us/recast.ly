8fbe5601488da7ef437dac72d063e66743a765de
cho319 <cho319us@gmail.com>
2020-07-15 17:02:29 -0700

    Project set up
    - npm install
    - Rendered the App component to the DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-// TODO: Render the `App` component to the DOM
+// import App component from the transplied file - App.js instead of App.jsx
 import App from './components/App.js';
 
+// Render the `App` component to the DOM
+ReactDOM.render(<App />, document.getElementById("app"));
